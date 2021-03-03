6d151b6bdef12a5c91b8d4b86c8805e246c9d08b
nicolas-choquet <nicolas-choquet@ssh1.paris1.alwaysdata.com>
2021-03-03 17:56:02 +0100

    ajout d'un wysiwyg

--- routes/recaps.php
+++ routes/recaps.php
@@ -3,7 +3,20 @@
 use Steampixel\Route;
 
 Route::add('/api/recap/upload', function() {
-    var_dump($_FILES['image']);
+    if (empty($_FILES['image']) === false) {
+        if (!is_dir(__DIR__.'/../assets/recaps/images')) {
+		mkdir(__DIR__.'/../assets/recaps/images', 0777, true);
+	}
+	$path = __DIR__.'/../assets/recaps/images/'.basename($_FILES['image']['name']);
+	if (move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
+		echo json_encode([
+			'success' => true,
+			'file' => $_SERVER['SERVER_PROTOCOL'].'//'.$_SERVER['SERVER_NAME'].'/assets/recaps/images/'.basename($_FILES['image']['name'])
+		]);
+	} else {
+		echo json_encode([ 'success' => false, 'message' => "Il y a eu un problème lors de l'upload" ]);
+	}
+    }
 }, 'post');
 
 Route::add('/api/recaps', function() {}, 'get');
