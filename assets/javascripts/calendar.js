46d9862baa1e47bf4d829cf2762415eded58157e
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-03 17:50:02 +0000

    ajout d'une couleur verte sur les jours que j'ai réservé

--- assets/javascripts/calendar.js
+++ assets/javascripts/calendar.js
@@ -55,106 +55,114 @@ function create_calendar(json) {
   }
   
   function create_current_month_day(day, row) {
-    let col = document.createElement('div');
-    col.classList.add(`col-2`, 'calendar-day');
-    const today = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate();
-    
-    if (today === day.date) {
-      col.classList.add('today');
-    }
     if (day.day >= 6) {
-      col.classList.remove('col-2');
-      col.classList.add('weekend', 'col-1');
+      if (day.day === 5) {
+        let col = document.createElement('div');
+        col.classList.add(`col-2`);
+        row.appendChild(col);
+      }
     } else {
-      col.addEventListener('click', e => {
-        if (e.target.tagName === 'DIV') {
-          console.log(day.date, day.reservations, day.presences);
-          
-          build_day_modal(new Date(day.date), day.reservations, day.presences);
-          
-          window.location.href = '#modal-calandar-day';
-        }
-      });
-    }
-    
-    const badges_container = document.createElement('div');
-    badges_container.classList.add('badges-container');
-    
-    if (day.reservations.length > 0) {
-      const badge = document.createElement('span');
-      badge.classList.add('nb-reservations');
-      badge.classList.add('badge', 'bg-secondary', 'text-light');
-      badge.innerHTML = day.reservations.length;
-      badges_container.appendChild(badge);
-    }
-    
-    const day_badge = document.createElement('span');
-    day_badge.classList.add('badge', 'rounded-pill', 'bg-info', 'text-dark');
-    day_badge.innerHTML = day.date.split('-')[2];
-    badges_container.appendChild(day_badge);
-    
-    col.appendChild(badges_container);
-    
-    const content = document.createElement('div');
-    if (day.day < 6) {
-      const reservation_btn = document.createElement('button');
-      reservation_btn.setAttribute('type', 'button');
-      reservation_btn.classList.add('btn', 'btn-primary', 'btn-sm');
+      let col = document.createElement('div');
+      col.classList.add(`col-2`, 'calendar-day');
+      const today = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate();
       
-      const iHaveReserved = day.reservations.reduce((reducer, curr) => {
-        if (curr.id_user === JSON.parse(localStorage.getItem('user')).id) {
-          reducer = true;
-        }
-        return reducer;
-      }, false);
-
-      if (iHaveReserved) {
-        col.classList.add('reserved');
+      if (today === day.date) {
+        col.classList.add('today');
+      }
+      /*if (day.day >= 6) {
+        col.classList.remove('col-2');
+        col.classList.add('weekend', 'col-1');
+      } else {*/
+        col.addEventListener('click', e => {
+          if (e.target.tagName === 'DIV') {
+            console.log(day.date, day.reservations, day.presences);
+            
+            build_day_modal(new Date(day.date), day.reservations, day.presences);
+            
+            window.location.href = '#modal-calandar-day';
+          }
+        });
+      //}
+      
+      const badges_container = document.createElement('div');
+      badges_container.classList.add('badges-container');
+      
+      if (day.reservations.length > 0) {
+        const badge = document.createElement('span');
+        badge.classList.add('nb-reservations');
+        badge.classList.add('badge', 'bg-secondary', 'text-light');
+        badge.innerHTML = day.reservations.length;
+        badges_container.appendChild(badge);
       }
       
-      if (day.reservations.length === 5 && !iHaveReserved) {
-        reservation_btn.setAttribute('disabled', 'disabled');
-        reservation_btn.classList.add('btn-disabled');
-      }
+      const day_badge = document.createElement('span');
+      day_badge.classList.add('badge', 'rounded-pill', 'bg-info', 'text-dark');
+      day_badge.innerHTML = day.date.split('-')[2];
+      badges_container.appendChild(day_badge);
       
-      reservation_btn.addEventListener('click', e => {
-        e.preventDefault();
+      col.appendChild(badges_container);
+      
+      const content = document.createElement('div');
+      if (day.day < 6) {
+        const reservation_btn = document.createElement('button');
+        reservation_btn.setAttribute('type', 'button');
+        reservation_btn.classList.add('btn', 'btn-primary', 'btn-sm');
         
-        fetch('/api/reservation', {
-          method: (iHaveReserved ? 'delete' : 'post'),
-          body: JSON.stringify({
-            user_id: JSON.parse(localStorage.getItem('user')).id,
-            date: day.date
-          })
-        }).then(r => r.json())
-          .then(json => {
-            if (!json.error) {
-              request_calendar(currentDate.getMonth() + 1, currentDate.getFullYear())
-            }
-          });
-      });
-      
-      const btn_content = document.createElement('span');
-      btn_content.classList.add('d-none', 'd-md-inline');
-      if (iHaveReserved) {
-        btn_content.innerHTML = 'Annuler';
-      } else {
-        btn_content.innerHTML = 'Réserver';
+        const iHaveReserved = day.reservations.reduce((reducer, curr) => {
+          if (curr.id_user === JSON.parse(localStorage.getItem('user')).id) {
+            reducer = true;
+          }
+          return reducer;
+        }, false);
+
+        if (iHaveReserved) {
+          col.classList.add('reserved');
+        }
+        
+        if (day.reservations.length === 5 && !iHaveReserved) {
+          reservation_btn.setAttribute('disabled', 'disabled');
+          reservation_btn.classList.add('btn-disabled');
+        }
+        
+        reservation_btn.addEventListener('click', e => {
+          e.preventDefault();
+          
+          fetch('/api/reservation', {
+            method: (iHaveReserved ? 'delete' : 'post'),
+            body: JSON.stringify({
+              user_id: JSON.parse(localStorage.getItem('user')).id,
+              date: day.date
+            })
+          }).then(r => r.json())
+            .then(json => {
+              if (!json.error) {
+                request_calendar(currentDate.getMonth() + 1, currentDate.getFullYear())
+              }
+            });
+        });
+        
+        const btn_content = document.createElement('span');
+        btn_content.classList.add('d-none', 'd-md-inline');
+        if (iHaveReserved) {
+          btn_content.innerHTML = 'Annuler';
+        } else {
+          btn_content.innerHTML = 'Réserver';
+        }
+        reservation_btn.appendChild(btn_content);
+        
+        const btn_icon = document.createElement('i');
+        btn_icon.classList.add('fas', 'd-inline', 'd-md-none');
+        if (iHaveReserved) {
+          btn_icon.classList.add('fa-user-minus');
+        } else {
+          btn_icon.classList.add('fa-user-plus');
+        }
+        reservation_btn.appendChild(btn_icon);
+        content.appendChild(reservation_btn);
       }
-      reservation_btn.appendChild(btn_content);
-      
-      const btn_icon = document.createElement('i');
-      btn_icon.classList.add('fas', 'd-inline', 'd-md-none');
-      if (iHaveReserved) {
-        btn_icon.classList.add('fa-user-minus');
-      } else {
-        btn_icon.classList.add('fa-user-plus');
-      }
-      reservation_btn.appendChild(btn_icon);
-      content.appendChild(reservation_btn);
+      col.appendChild(content);
+      row.appendChild(col);
     }
-    col.appendChild(content);
-    row.appendChild(col);
   }
   
   function create_externe_month_day(json, last_day, date_day, additional_class, row) {
