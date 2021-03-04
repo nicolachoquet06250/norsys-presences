4a7e1ba7d690af57a2cf9c0c4feddfb29e660c7c
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-04 08:50:26 +0000

    ajout d'un wysiwyg

--- assets/javascripts/recap.js
+++ assets/javascripts/recap.js
@@ -10,4 +10,42 @@ window.addEventListener('load', () => {
     const user = JSON.parse(localStorage.getItem('user'));
     document.querySelector('#profile span').innerHTML = user.lastname + ' ' + user.firstname.substr(0, 1) + '.';
     document.querySelector('#profile span + strong').innerHTML = user.agency;
+
+    
+
+    $('#editor').trumbowyg({
+        btnsDef: {
+            // Create a new dropdown
+            image: {
+                dropdown: ['insertImage', 'upload'],
+                ico: 'insertImage'
+            }
+        },
+        // Redefine the button pane
+        btns: [
+            ['viewHTML'],
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['image'], // Our fresh created dropdown
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen'],
+            ['emoji']
+        ],
+        plugins: {
+            // Add imagur parameters to upload plugin for demo purposes
+            upload: {
+                serverPath: '/api/recap/upload',
+                fileFieldName: 'image',
+                headers: {
+                    'Authorization': JSON.parse(localStorage.getItem('user')).token
+                },
+                urlPropertyName: 'file'
+            }
+        }
+    });
 });
