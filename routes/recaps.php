f06d9512d71caa30afedd99b9e99be78122ab71b
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-04 17:53:49 +0000

    mise à jour du template

--- routes/recaps.php
+++ routes/recaps.php
@@ -24,7 +24,11 @@ Route::add('/api/recaps', function() {}, 'get');
 Route::add('/api/recap/([0-9]+)', function($id) {}, 'get');
 
 Route::add('/api/recap', function() {
-	echo getBody()['html'];
+	$body = getBody();
+	$token = $body['token'];
+	$user = json_decode(base64_decode($token));
+
+	echo $body['html'] . '<pre>' . $user . '</pre>';
 }, 'post');
 
 Route::add('/api/recap/([0-9]+)', function() {}, 'put');
