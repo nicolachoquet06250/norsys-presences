2836785644b8ecb36c6d373edc25f4fd1911ae72
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-04 19:29:41 +0000

    mise à jour du template

--- routes/recaps.php
+++ routes/recaps.php
@@ -27,82 +27,91 @@ Route::add('/api/recap', function() {
 	$monthes = ['', 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'septembre', 'Octobre', 'Novembre', 'Décembre'];
 
 	$body = getBody();
-	$token = $body['token'];
-	$user = json_decode(base64_decode($token), true);
 
-	$email = $user['email'];
+	if (empty($body['html']) === false) {
+		$token = $body['token'];
+		$user = json_decode(base64_decode($token), true);
 
-	$agency = $user['agency'];
+		$email = $user['email'];
 
-	$week = get_week_extremity_days(date('W'), date('Y'));
+		$agency = $user['agency'];
 
-	$start = date('d', strtotime($week['first_day']));
-	$end = date('d', strtotime($week['last_day']));
-	$month = $monthes[intval(date('n', strtotime($week['first_day'])))];
+		$week = get_week_extremity_days(date('W'), date('Y'));
 
-	$object = 'Récap Hebdo Sophia ' . date('d/m/Y');
+		$start = date('d', strtotime($week['first_day']));
+		$end = date('d', strtotime($week['last_day']));
+		$month = $monthes[intval(date('n', strtotime($week['first_day'])))];
 
-	$agency_users = [];
+		$object = 'Récap Hebdo Sophia ' . date('d/m/Y');
 
-	try {
-		$db = getDB();
+		$agency_users = [];
 
-		/*$request = $db->prepare('SELECT * FROM `users` WHERE agency_id = ( SELECT id FROM agencies WHERE name = :agency )', [ PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY ]);
-		$request->execute([ 'agency' => $agency ]);
-		$agency_users = $request->fetchAll(PDO::FETCH_ASSOC);*/
+		try {
+			$db = getDB();
 
-		$agency_users = [
-			[
-				'email' => $email
-			]
-		];
+			/*$request = $db->prepare('SELECT * FROM `users` WHERE agency_id = ( SELECT id FROM agencies WHERE name = :agency )', [ PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY ]);
+			$request->execute([ 'agency' => $agency ]);
+			$agency_users = $request->fetchAll(PDO::FETCH_ASSOC);*/
 
-		$request = $db->prepare('INSERT INTO recap (`user_id`, `agency_id`, `object`, `content`) VALUES(:user_id, (SELECT id FROM agencies WHERE name = :agency), :object, :content)', [ PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY ]);
-		$request->execute([
-			'user_id' => $user['id'],
-			'agency' => $user['agency'],
-			'object' => $object,
-			'content' => $body['html']
-		]);
+			$agency_users = [
+				[
+					'email' => $email
+				]
+			];
 
-		$html = '
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<title>Norsys Sophia | Fiche de présence</title>
-			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" 
-				rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" 
-				crossorigin="anonymous">
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.23.0/ui/trumbowyg.min.css" />
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.19.1/plugins/emoji/ui/trumbowyg.emoji.css" />
-	</head>
-	<body>' . str_replace(['%start%', '%end%', '%month%'], [$start, $end, $month], $body['html']) . '</body>
-	</html>';
+			$request = $db->prepare('INSERT INTO recap (`user_id`, `agency_id`, `object`, `content`) VALUES(:user_id, (SELECT id FROM agencies WHERE name = :agency), :object, :content)', [ PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY ]);
+			$request->execute([
+				'user_id' => $user['id'],
+				'agency' => $user['agency'],
+				'object' => $object,
+				'content' => $body['html']
+			]);
 
-		$mails = [];
+			$html = '
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<title>Norsys Sophia | Fiche de présence</title>
+				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" 
+					rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" 
+					crossorigin="anonymous">
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.23.0/ui/trumbowyg.min.css" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.19.1/plugins/emoji/ui/trumbowyg.emoji.css" />
+		</head>
+		<body>' . str_replace(['%start%', '%end%', '%month%'], [$start, $end, $month], $body['html']) . '</body>
+		</html>';
 
-		foreach ($agency_users as $user) {
-			$mails[$user['email']] = smtpMailer($user['email'], $html, $object);
-		}
+			$mails = [];
 
-		foreach ($mails as $mail) {
-			if ($mail !== true) {
-				exit(json_encode([
-					'error' => true,
-					'message' => $mail
-				]));
+			foreach ($agency_users as $user) {
+				$mails[$user['email']] = smtpMailer($user['email'], $html, $object);
 			}
+
+			foreach ($mails as $mail) {
+				if ($mail !== true) {
+					exit(json_encode([
+						'error' => true,
+						'message' => $mail
+					]));
+				}
+			}
+
+			echo json_encode([
+				'error' => false
+			]);
+		} catch (Exception $e) {
+			exit(json_encode([
+				'error' => true,
+				'message' => $e->getMessage()
+			]));
 		}
-
-		echo json_encode([
-			'error' => false
-		]);
-	} catch (Exception $e) {
-		
+	} else {
+		exit(json_encode([
+			'error' => true,
+			'message' => 'Vous devez remplire le message avant de l\'envoyer...'
+		]));
 	}
-
-	//echo str_replace(['%start%', '%end%', '%month%'], [$start, $end, $month], $body['html']);
 
 }, 'post');
 
