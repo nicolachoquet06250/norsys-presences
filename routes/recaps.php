976f1184a233a8e3b351957075f20ce761246625
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-05 07:41:00 +0000

    mise à jour du template

--- routes/recaps.php
+++ routes/recaps.php
@@ -5,23 +5,91 @@ use Steampixel\Route;
 Route::add('/api/recap/upload', function() {
     if (empty($_FILES['image']) === false) {
         if (!is_dir(__DIR__.'/../assets/recaps/images')) {
-		mkdir(__DIR__.'/../assets/recaps/images', 0777, true);
-	}
-	$path = __DIR__.'/../assets/recaps/images/'.basename($_FILES['image']['name']);
-	if (move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
-		echo json_encode([
-			'success' => true,
-			'file' => 'https://'.$_SERVER['SERVER_NAME'].'/assets/recaps/images/'.basename($_FILES['image']['name'])
-		]);
-	} else {
-		echo json_encode([ 'success' => false, 'message' => "Il y a eu un problème lors de l'upload" ]);
-	}
+			mkdir(__DIR__.'/../assets/recaps/images', 0777, true);
+		}
+
+		$path = __DIR__.'/../assets/recaps/images/'.basename($_FILES['image']['name']);
+
+		if (move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
+			echo json_encode([
+				'success' => true,
+				'file' => 'https://'.$_SERVER['SERVER_NAME'].'/assets/recaps/images/'.basename($_FILES['image']['name'])
+			]);
+		} else {
+			echo json_encode([ 'success' => false, 'message' => "Il y a eu un problème lors de l'upload" ]);
+		}
     }
 }, 'post');
 
-Route::add('/api/recaps', function() {}, 'get');
+Route::add('/api/recaps/([^\/]+)', function($token) {
+	try {
+		if (empty($token)) {
+			throw new Exception('Authentification invalide');
+		}
+		$token = base64_decode($token);
+		if ($token) {
+			$token = json_decode($token, true);
 
-Route::add('/api/recap/([0-9]+)', function($id) {}, 'get');
+			if (json_last_error()) {
+				throw new Exception(json_last_error_msg());
+			}
+		}
+
+		$db = getDB();
+		$request = $db->prepare('SELECT recap.id recap_id, 
+										DATE_FORMAT(creation_date, \'%Y-%m-%d\') creation_date, 
+										content, object, 
+										users.firstname, users.lastname, users.email 
+											FROM recap INNER JOIN users 
+												ON recap.user_id = users.id 
+												WHERE recap.agency_id = (
+													SELECT id FROM agencies WHERE name = :agency
+												)', [ PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY ]);
+		$request->execute([ 'agency' => $token['agency'] ]);
+		$result = $request->fetchAll(PDO::FETCH_ASSOC);
+
+		echo json_encode($result);
+	} catch (Exception $e) {
+		exit(json_encode([
+			'error' => true,
+			'message' => $e->getMessage()
+		]));
+	}
+}, 'get');
+
+Route::add('/api/recap/([0-9]+)/([^\/]+)', function($id, $token) {
+	try {
+		if (empty($token)) {
+			throw new Exception('Authentification invalide');
+		}
+		$token = base64_decode($token);
+		if ($token) {
+			$token = json_decode($token, true);
+
+			if (json_last_error()) {
+				throw new Exception(json_last_error_msg());
+			}
+		}
+
+		$db = getDB();
+		$request = $db->prepare('SELECT recap.id recap_id, 
+										DATE_FORMAT(creation_date, \'%Y-%m-%d\') creation_date, 
+										content, object, 
+										users.firstname, users.lastname, users.email 
+											FROM recap INNER JOIN users 
+												ON recap.user_id = users.id 
+												WHERE recap.agency_id = (
+													SELECT id FROM agencies WHERE name = :agency
+												) AND id = :id', [ PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY ]);
+		$request->execute([
+			'agency' => $token['agency'],
+			'id' => $id
+		]);
+		$result = $request->fetchAll(PDO::FETCH_ASSOC);
+	} catch (Exception $e) {
+
+	}
+}, 'get');
 
 Route::add('/api/recap', function() {
 	$monthes = ['', 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'septembre', 'Octobre', 'Novembre', 'Décembre'];
@@ -114,10 +182,6 @@ Route::add('/api/recap', function() {
 	}
 
 }, 'post');
-
-Route::add('/api/recap/([0-9]+)', function() {}, 'put');
-
-Route::add('/api/recap/([0-9]+)', function() {}, 'delete');
 
 Route::add('/api/recap/save-template', function() {
 	$body = getBody();
