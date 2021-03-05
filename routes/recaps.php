5248d322fa5ad642d32adb55d5ec53c5995dc57b
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-05 10:20:33 +0000

    mise à jour du template

--- routes/recaps.php
+++ routes/recaps.php
@@ -157,7 +157,7 @@ Route::add('/api/recap', function() {
 
 			$html = '
 		<!DOCTYPE HTML PUBLIC "~//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-		<html lang="fr">
+		<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 				<title>Norsys Sophia | Fiche de présence</title>
