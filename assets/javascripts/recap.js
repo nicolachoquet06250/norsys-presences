889dfa84283a368eaf9d149ca0b51869dcbd6f59
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-05 16:56:59 +0000

    mise en place de l'envoie des récaps hebdo sur une mailing_list

--- assets/javascripts/recap.js
+++ assets/javascripts/recap.js
@@ -17,113 +17,113 @@ document.querySelector('#logout').addEventListener('click', () => {
 });
 
 window.addEventListener('load', () => {
-    const today = new Date();
-    const days = ['', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
-    const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
-    document.querySelector('#today').innerHTML = days[today.getDay()] + ' ' + today.getDate() + ' ' + months[today.getMonth()] + ' ' + today.getFullYear();
+const today = new Date();
+const days = ['', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+document.querySelector('#today').innerHTML = days[today.getDay()] + ' ' + today.getDate() + ' ' + months[today.getMonth()] + ' ' + today.getFullYear();
 
 
-    const user = JSON.parse(localStorage.getItem('user'));
-    document.querySelector('#profile span').innerHTML = user.lastname + ' ' + user.firstname.substr(0, 1) + '.';
-    document.querySelector('#profile span + strong').innerHTML = user.agency;
+const user = JSON.parse(localStorage.getItem('user'));
+document.querySelector('#profile span').innerHTML = user.lastname + ' ' + user.firstname.substr(0, 1) + '.';
+document.querySelector('#profile span + strong').innerHTML = user.agency;
 
-    if (document.querySelector('#editor')) {
-        fetch('/views/recap_templates/hebdo.html')
-            .then(r => r.text())
-            .then(html => {
-                $('#editor').trumbowyg({
-                    btnsDef: {
-                        // Create a new dropdown
-                        image: {
-                            dropdown: ['insertImage', 'upload'],
-                            ico: 'insertImage'
-                        }
-                    },
-                    btns: [
-                        ['viewHTML'],
-                        ['formatting'],
-                        ['strong', 'em', 'del'],
-                        ['superscript', 'subscript'],
-                        ['link'],
-                        ['image'],
-                        ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-                        ['unorderedList', 'orderedList'],
-                        ['horizontalRule'],
-                        ['removeformat'],
-                        ['fullscreen'],
-                        ['emoji'],
-                        ['template']
-                    ],
-                    plugins: {
-                        upload: {
-                            serverPath: '/api/recap/upload',
-                            fileFieldName: 'image',
-                            headers: {
-                                'Authorization': JSON.parse(localStorage.getItem('user')).token
-                            },
-                            urlPropertyName: 'file'
+if (document.querySelector('#editor')) {
+    fetch('/views/recap_templates/hebdo.html')
+        .then(r => r.text())
+        .then(html => {
+            $('#editor').trumbowyg({
+                btnsDef: {
+                    // Create a new dropdown
+                    image: {
+                        dropdown: ['insertImage', 'upload'],
+                        ico: 'insertImage'
+                    }
+                },
+                btns: [
+                    ['viewHTML'],
+                    ['formatting'],
+                    ['strong', 'em', 'del'],
+                    ['superscript', 'subscript'],
+                    ['link'],
+                    ['image'],
+                    ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+                    ['unorderedList', 'orderedList'],
+                    ['horizontalRule'],
+                    ['removeformat'],
+                    ['fullscreen'],
+                    ['emoji'],
+                    ['template']
+                ],
+                plugins: {
+                    upload: {
+                        serverPath: '/api/recap/upload',
+                        fileFieldName: 'image',
+                        headers: {
+                            'Authorization': JSON.parse(localStorage.getItem('user')).token
                         },
-                        templates: [
-                            { name: 'Récap hebdo', html }
-                        ]
-                    }
-                });
+                        urlPropertyName: 'file'
+                    },
+                    templates: [
+                        { name: 'Récap hebdo', html }
+                    ]
+                }
             });
-    }
-
-    if (document.querySelector('#send_recap')) {
-        document.querySelector('#send_recap').addEventListener('click', e => {
-            e.preventDefault()
-            e.stopPropagation();
-
-            document.querySelector('.loader').classList.remove('d-none');
-            document.querySelector('.loader').classList.add('d-flex');
-
-            fetch('/api/recap', {
-                    method: 'post',
-                    body: JSON.stringify({
-                        html: document.querySelector('#editor').innerHTML,
-                        token: JSON.parse(localStorage.getItem('user')).token
-                    })
-                }).then(r => r.json())
-                .then(json => {
-                    if (!json.error) {
-                        document.querySelector('.loader').classList.add('d-none');
-                        document.querySelector('.loader').classList.remove('d-flex');
-                    }
-                });
         });
-    }
+}
 
-    if (document.querySelector('#save_template')) {
-        document.querySelector('#save_template').addEventListener('click', e => {
-            e.preventDefault();
-            e.stopPropagation();
+if (document.querySelector('#send_recap')) {
+    document.querySelector('#send_recap').addEventListener('click', e => {
+        e.preventDefault()
+        e.stopPropagation();
 
-            document.querySelector('.loader').classList.remove('d-none');
-            document.querySelector('.loader').classList.add('d-flex');
+        document.querySelector('.loader').classList.remove('d-none');
+        document.querySelector('.loader').classList.add('d-flex');
 
-            fetch('/api/recap/save-template', {
-                    method: 'put',
-                    body: JSON.stringify({
-                        html: document.querySelector('#editor').innerHTML
-                    })
-                }).then(r => r.json())
-                .then(json => {
-                    if (!json.error) {
-                        document.querySelector('.loader').classList.add('d-none');
-                        document.querySelector('.loader').classList.remove('d-flex');
-                    }
+        fetch('/api/recap', {
+                method: 'post',
+                body: JSON.stringify({
+                    html: document.querySelector('#editor').innerHTML,
+                    token: JSON.parse(localStorage.getItem('user')).token
                 })
-        });
-    }
-
-    if (document.querySelector('.historique-recap-container')) {
-        const token = JSON.parse(localStorage.getItem('user')).token;
-        fetch(`/api/recaps/${token}`)
-            .then(r => r.json())
+            }).then(r => r.json())
             .then(json => {
-                const template = recap => `
+                if (!json.error) {
+                    document.querySelector('.loader').classList.add('d-none');
+                    document.querySelector('.loader').classList.remove('d-flex');
+                }
+            });
+    });
+}
+
+if (document.querySelector('#save_template')) {
+    document.querySelector('#save_template').addEventListener('click', e => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        document.querySelector('.loader').classList.remove('d-none');
+        document.querySelector('.loader').classList.add('d-flex');
+
+        fetch('/api/recap/save-template', {
+                method: 'put',
+                body: JSON.stringify({
+                    html: document.querySelector('#editor').innerHTML
+                })
+            }).then(r => r.json())
+            .then(json => {
+                if (!json.error) {
+                    document.querySelector('.loader').classList.add('d-none');
+                    document.querySelector('.loader').classList.remove('d-flex');
+                }
+            })
+    });
+}
+
+if (document.querySelector('.historique-recap-container')) {
+    const token = JSON.parse(localStorage.getItem('user')).token;
+    fetch(`/api/recaps/${token}`)
+        .then(r => r.json())
+        .then(json => {
+            const template = recap => `
                 <div class="col-12 col-md-6">
                     <div class="card">
                         <div class="card-body">
@@ -133,28 +133,39 @@ window.addEventListener('load', () => {
                         </div>
                     </div>
                 </div>`;
-                document.querySelector('.historique-recap-container').innerHTML = '';
-                for (let recap of json) {
-                    document.querySelector('.historique-recap-container').innerHTML += template(recap);
-                }
-            })
-    }
+            const void_template = () => `
+            <div class="col-12 d-flex justify-content-center">
+                <strong>Aucun Récap hebdo n'à précédement été enregistré</strong>
+            </div>`;
+            document.querySelector('.historique-recap-container').innerHTML = '';
+            if (json.length === 0) {
 
-    if (window.location.pathname === '/recap') {
-        if (parseQueryString().id) {
-            const token = JSON.parse(localStorage.getItem('user')).token;
-            fetch(`/api/recap/${parseQueryString().id}/${token}`)
-                .then(r => r.json())
-                .then(json => {
-                    let content = json.content;
-                    for (let variable of Object.keys(json.vars)) {
-                        content = content.replace(`%${variable}%`, json.vars[variable]);
-                    }
-                    document.querySelector('#object').innerHTML = json.object;
-                    document.querySelector('#content').innerHTML = content;
-                })
-        } else {
-            window.location.href = '/recap/list';
-        }
+            }
+
+            for (let recap of json) {
+                document.querySelector('.historique-recap-container').innerHTML += template(recap);
+            }
+        })
+}
+
+if (window.location.pathname === '/recap') {
+    if (parseQueryString().id) {
+        const token = JSON.parse(localStorage.getItem('user')).token;
+        fetch(`/api/recap/${parseQueryString().id}/${token}`)
+            .then(r => r.json())
+            .then(json => {
+                let content = json.content;
+                for (let variable of Object.keys(json.vars)) {
+                    content = content.replace(`%${variable}%`, json.vars[variable]);
+                }
+                document.querySelector('#object').innerHTML = json.object;
+                document.querySelector('#content').innerHTML = content;
+            })
+    } else {
+        window.location.href = '/recap/list';
     }
+}
+});
+}
+}
 });
