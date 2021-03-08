daa00e7d504ead0e2f7ef071e727df849e0970f4
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-08 13:03:34 +0000

    test

--- mail.php
+++ mail.php
@@ -1,28 +1,38 @@
 <?php
 
+require "vendor/autoload.php";
+
+use \PHPMailer\PHPMailer\PHPMailer;
+use \PHPMailer\PHPMailer\SMTP;
+use \PHPMailer\PHPMailer\POP3;
+
 try {
+    $pop = new POP3(); /*Create a new object for pop3*/
+    $pop->authorise('outlook.office365.com', 995, 30, 'nchoquet@norsys.fr', '12041998Yann?', 1); /*login in to pop3 */
+
     $mail = new PHPMailer();  // Cree un nouvel objet PHPMailer
     $mail->IsSMTP(); // active SMTP
     $mail->isHTML(true);
-    $mail->SMTPDebug = SMTP::DEBUG_OFF;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
-    $mail->SMTPAuth = true;  // Authentification SMTP active
-    $mail->SMTPSecure = false; // Gmail REQUIERT Le transfert securise
-    if (defined(EMAIL_ENCRIPTION) && EMAIL_ENCRIPTION) {
-        $mail->SMTPSecure = EMAIL_ENCRIPTION;
-    }
-    $mail->Host = EMAIL_HOST;
-    $mail->Port = EMAIL_PORT;
-    $mail->Username = EMAIL;
-    $mail->Password = EMAIL_PASSWORD;
-    $mail->SetFrom($mail->Username, EMAIL_NAME);
-    $mail->Subject = $subject;
-    $mail->Body = $body;
-    $mail->AddAddress($to);
-    $mail->CharSet = 'UTF-8';
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true)
+       ); /*Skip SSL Errors(if any),generally not needed*/
+    //$mail->SMTPDebug = SMTP::DEBUG_OFF;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
+    //$mail->SMTPAuth = true;  // Authentification SMTP active
+    //$mail->SMTPSecure = false; // Gmail REQUIERT Le transfert securise
+    $mail->SMTPSecure = 'tls';
 
-    foreach ($attachments as $attachment) {
-        $mail->addAttachment($attachment); 
-    }
+    $mail->Host = 'smtp.office365.com';
+    //$mail->Port = 587;
+    //$mail->Username = 'nchoquet@norsys.com';
+    //$mail->Password = '12041998Yann?';
+    $mail->SetFrom('nchoquet@norsys.fr', 'Nicolas CHOQUET');
+    $mail->Subject = 'subject';
+    $mail->Body = 'body';
+    $mail->AddAddress('nchoquet@norsys.fr');
+    $mail->CharSet = 'UTF-8';
 
     if($mail->Send()) {
         var_dump('success');
