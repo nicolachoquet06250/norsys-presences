5814fe3d29a250fdeae20f4b9162f00029d74ee2
Choquet Nicolas <nicolachoquet06250@gmail.com>
2021-03-18 07:46:59 +0000

    donne l'accès au template du récap hebdo à tout le monde

--- routes/front.php
+++ routes/front.php
@@ -31,5 +31,5 @@ Route::add('/recap/list', function() {
 
 Route::add('/templates/hebdo.html', function() {
   header('Access-Control-Allow-Origin: *');
-  $content = file_get_contents(__DIR__.'../views/recap_templates/hebdo.html');
+  $content = file_get_contents(__DIR__.'/../views/recap_templates/hebdo.html');
 }, 'get');
